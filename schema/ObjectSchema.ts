84fa93ef1a48940e295f7fbb7fe7e25ebe7d38dd
sowdri <sowdri@gmail.com>
2020-02-12 17:36:20 +1030

    Updated object schema

--- schema/ObjectSchema.ts
+++ schema/ObjectSchema.ts
@@ -1,5 +1,8 @@
 import { Schema } from "./Schema";
+import { BaseSchema } from "./BaseSchema";
 
-export type ObjectSchema<T> = { __required: boolean } & {
-  [P in keyof T]: Schema<T[P]>;
+export type ObjectSchema<T> = BaseSchema & {
+  properties: {
+    [P in keyof T]: Schema<T[P]>;
+  };
 };

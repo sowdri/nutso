ad07257086f39ee78012f1fce76731d85132d0e0
sowdri <sowdri@gmail.com>
2020-02-12 17:36:20 +1030

    Updated object schema

--- tests/data/sample.ts
+++ tests/data/sample.ts
@@ -18,35 +18,62 @@ interface Address {
 }
 
 export const customerSchema1: Schema<Customer> = {
-  __required: false,
-  name: {
-    minLength: 3,
-    maxLength: 24,
-    required: true
-  },
-  sex: {
-    required: true,
-    regex: /male|female/
-  },
-  dob: {
-    required: false
-  },
-  age: {
-    min: 0,
-    max: 100,
-    required: true
-  },
-  favouriteColors: {
-    minItems: 1,
-    maxItems: 4,
-    required: false,
-    item: {
-      regex: /(blue)|(green)/,
+  required: false,
+  properties: {
+    name: {
+      minLength: 3,
+      maxLength: 24,
       required: true
+    },
+    sex: {
+      required: true,
+      regex: /male|female/
+    },
+    dob: {
+      required: false
+    },
+    age: {
+      min: 0,
+      max: 100,
+      required: true
+    },
+    favouriteColors: {
+      minItems: 1,
+      maxItems: 4,
+      required: false,
+      item: {
+        regex: /(blue)|(green)/,
+        required: true
+      }
+    },
+    address: {
+      required: false,
+      properties: {
+        line1: {
+          required: false
+        },
+        city: {
+          required: false
+        },
+        state: {
+          required: false
+        },
+        country: {
+          required: true
+        },
+        postcode: {
+          required: true,
+          min: 4999,
+          max: 6000
+        }
+      }
     }
-  },
-  address: {
-    __required: false,
+  }
+};
+
+export const addressSchema1: Schema<Address> = {
+  required: false,
+  properties: {
     line1: {
       required: false
     },
@@ -67,54 +94,35 @@ export const customerSchema1: Schema<Customer> = {
   }
 };
 
-export const addressSchema1: Schema<Address> = {
-  __required: false,
-  line1: {
-    required: false
-  },
-  city: {
-    required: false
-  },
-  state: {
-    required: false
-  },
-  country: {
-    required: true
-  },
-  postcode: {
-    required: true,
-    min: 4999,
-    max: 6000
-  }
-};
-
 export const customerSchema2: Schema<Customer> = {
-  __required: false,
-  name: {
-    minLength: 3,
-    maxLength: 24,
-    required: true
-  },
-  sex: {
-    required: true,
-    regex: /male|female/
-  },
-  dob: {
-    required: false
-  },
-  age: {
-    min: 0,
-    max: 100,
-    required: true
-  },
-  favouriteColors: {
-    minItems: 1,
-    maxItems: 4,
-    required: false,
-    item: {
-      regex: /(blue)|(green)/,
+  required: false,
+  properties: {
+    name: {
+      minLength: 3,
+      maxLength: 24,
       required: true
-    }
-  },
-  address: addressSchema1
+    },
+    sex: {
+      required: true,
+      regex: /male|female/
+    },
+    dob: {
+      required: false
+    },
+    age: {
+      min: 0,
+      max: 100,
+      required: true
+    },
+    favouriteColors: {
+      minItems: 1,
+      maxItems: 4,
+      required: false,
+      item: {
+        regex: /(blue)|(green)/,
+        required: true
+      }
+    },
+    address: addressSchema1
+  }
 };

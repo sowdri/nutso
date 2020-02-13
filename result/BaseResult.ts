fd564c70c02850cf9419d40fa43b74b4db398c83
Sowdri Murugesan <sowdri@restoplus.com.au>
2020-02-13 11:31:48 +1030

    Created date validation

--- result/BaseResult.ts
+++ result/BaseResult.ts
@@ -1,4 +1,7 @@
+import { FieldPath } from "../utils/FieldPath";
+
 export type BaseResult = {
   isValid: boolean;
   errorMessage: string;
+  fieldPath: FieldPath;
 };
--- validate/validateArray.ts
+++ validate/validateArray.ts
@@ -1,47 +1,47 @@
-import { ObjectResult } from "../result/ObjectResult";
-import { ObjectSchema } from "../schema/ObjectSchema";
-import { FieldPath, fieldPathStr } from "../utils/FieldPath";
+import { ArrayResult } from "../result/ArrayResult";
+import { ArraySchema } from "../schema/ArraySchema";
+import { FieldPath } from "../utils/FieldPath";
 import { isNil } from "../utils/is";
 import { validate } from "./validate";
-import { ArraySchema } from "../schema/ArraySchema";
-import { ArrayResult } from "../result/ArrayResult";
 
 export const validateArray = <T>(arr: T[], schema: ArraySchema<T>, fieldPath: FieldPath): ArrayResult<T> => {
   //
-  const path = fieldPathStr(fieldPath);
 
   // isnil
   if (isNil(arr)) {
     if (schema.required) {
       return {
         isValid: false,
-        errorMessage: `${path} is required.`,
-        items: []
+        errorMessage: `Required field.`,
+        items: [],
+        fieldPath
       };
     }
     return {
       isValid: true,
       errorMessage: ``,
-      items: []
+      items: [],
+      fieldPath
     };
   }
 
   const result: ArrayResult<T> = {
     isValid: true,
     errorMessage: "",
-    items: []
+    items: [],
+    fieldPath
   };
 
   // array min-items
   if (!isNil(schema.minItems) && arr.length < schema.minItems) {
     result.isValid = false;
-    result.errorMessage = `${path} should have at least ${schema.minItems} items.`;
+    result.errorMessage = `Should have at least ${schema.minItems} items.`;
   }
 
   // array max-items
   if (!isNil(schema.maxItems) && arr.length >= schema.maxItems) {
     result.isValid = false;
-    result.errorMessage = `${path} should not have more than ${schema.maxItems - 1} items.`;
+    result.errorMessage = `Should not have more than ${schema.maxItems - 1} items.`;
   }
 
   // for each key, validate

b99627dc342b0149e0ff026890e79b81a05112e6
Sowdri Murugesan <sowdri@restoplus.com.au>
2020-02-13 16:31:10 +1030

    Added test files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export * from "./validate/validate";
 export * from "./schema/Schema";
 export * from "./result/Result";
+
+export function validate() {
+  console.log("hello from nutso 🦸🏼‍♂️");
+}
+
+export const version = "1.1.13";

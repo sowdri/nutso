d75a203551a36652d647a418edc7733370412075
Sowdri Murugesan <sowdri@restoplus.com.au>
2020-02-13 17:03:04 +1030

    Exposed nutso again

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-// export * from "./validate/validate";
-// export * from "./schema/Schema";
-// export * from "./result/Result";
-
-export default {
-  foo: "bar"
-};
-
-export function validate() {
-  console.log("hello from nutso 🦸🏼‍♂️");
-}
-
-export const version = "1.1.13";
+export * from "./validate/validate";
+export * from "./schema/Schema";
+export * from "./result/Result";

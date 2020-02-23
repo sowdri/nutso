083df3f5924ee7d6b0efbe8ae4da38327fb73229
sowdri <sowdri@gmail.com>
2020-02-23 10:57:25 +1030

    Created new test for array

--- src/__tests__/TestValidateArray.spec.ts
+++ src/__tests__/TestValidateArray.spec.ts
@@ -32,3 +32,20 @@ test(`Test simple array values`, () => {
   expect(result).toMatchSnapshot();
   expect(result.items[0].isValid).toBe(false);
 });
+
+test(`Test array is valid, but one item is not`, () => {
+  type Colors = string[];
+  const favColors: Colors = ["blue", "green", "black"];
+  const colorsSchema: Schema<Colors> = {
+    type: "array",
+    minItems: 3,
+    maxItems: 10,
+    items: {
+      type: "string",
+      minLength: 5 // blue will fail
+    }
+  };
+  const result = validate(favColors, colorsSchema);
+  expect(result).toMatchSnapshot();
+  expect(result.items[0].isValid).toBe(false);
+});

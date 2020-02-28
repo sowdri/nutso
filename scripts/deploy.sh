1f9563dc8c0d4a225d3848e39a1dbe29eb5aafd7
sowdri <sowdri@gmail.com>
2020-02-29 08:17:42 +1030

    Added skip_cleanup to travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,5 @@ git remote rm origin
 git remote add origin https://sowdri:${GITHUB_TOKEN}@github.com/sowdri/nutso.git
 git symbolic-ref HEAD refs/heads/master
 
-npm run build
-
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
 npm run release

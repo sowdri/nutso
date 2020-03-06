07775ff1c7f89bd7ef9fda035f783a3ab40f4d06
sowdri <sowdri@gmail.com>
2020-03-06 21:23:33 +1030

    Added new test

--- src/__tests__/TestValidateObject.spec.ts
+++ src/__tests__/TestValidateObject.spec.ts
@@ -2,11 +2,11 @@ import { ObjectSchema } from "../models/schema/ObjectSchema";
 import { validateObject } from "../validate/validateObject";
 import { validate } from "../validate/validate";
 
-type Customer = {
-  name: "";
-};
-
 test(`Basic`, () => {
+  type Customer = {
+    name: "";
+  };
+
   const obj: Customer = { name: "" };
   const schema: ObjectSchema<Customer> = {
     type: "object",
@@ -23,4 +23,35 @@ test(`Basic`, () => {
 
   const result2 = validate(obj, schema);
   expect(result2.isValid).toBe(false);
+});
+
+test(`Address is optional in schema and undefined in object`, () => {
+  type Customer = {
+    name: "";
+    address?: {
+      city: string;
+    };
+  };
+
+  const obj: Customer = { name: "" };
+  const schema: ObjectSchema<Customer> = {
+    type: "object",
+    properties: {
+      name: {
+        type: "string",
+        minLength: 3
+      },
+      address: {
+        type: "object",
+        properties: {
+          city: {
+            type: "string"
+          }
+        }
+      }
+    }
+  };
+
+  const result = validateObject(obj, schema);
+  expect(result).toMatchSnapshot();
 });

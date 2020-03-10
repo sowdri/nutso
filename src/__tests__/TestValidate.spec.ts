a4674f7df96ccd38ff9d64bbccf481ad408c5247
Sowdri Murugesan <sowdri@restoplus.com.au>
2020-03-10 16:29:24 +1030

    Working index type matches

--- src/__tests__/TestValidate.spec.ts
+++ src/__tests__/TestValidate.spec.ts
@@ -1,15 +1,14 @@
 import { Schema } from "../models/schema/Schema";
-import { _validate } from "../validate/validate";
-
-interface Customer {
-  name: string;
-  address: Address;
-}
-interface Address {
-  line1: string;
-}
+import { _validate, validate } from "../validate/validate";
 
 test(`Test customer schema`, () => {
+  interface Customer {
+    name: string;
+    address: Address;
+  }
+  interface Address {
+    line1: string;
+  }
   const customerSchema: Schema<Customer> = {
     type: "object",
     optional: false,
@@ -36,4 +35,66 @@ test(`Test customer schema`, () => {
     }
   };
   expect(_validate(customer, customer, customerSchema)).toMatchSnapshot();
+});
+
+test(`Test index type`, () => {
+  type Car = {
+    vin: string;
+  };
+
+  interface Customer {
+    name: string;
+    address: Address;
+    cars: {
+      [key: string]: Car;
+    };
+  }
+  interface Address {
+    line1: string;
+  }
+  const customerSchema: Schema<Customer> = {
+    type: "object",
+    optional: false,
+    properties: {
+      name: {
+        type: "string",
+        minLength: 3,
+        maxLength: 24
+      },
+      address: {
+        type: "object",
+        properties: {
+          line1: {
+            type: "string"
+          }
+        }
+      },
+      cars: {
+        type: "object",
+        properties: {
+          rugby: {
+            type: "object",
+            properties: {
+              vin: {
+                type: "string"
+              }
+            }
+          }
+        }
+      }
+    }
+  };
+  const customer: Customer = {
+    name: "nish",
+    address: {
+      line1: "18/19 Lightsview"
+    },
+    cars: {
+      rugby: {
+        vin: "xsb797"
+      }
+    }
+  };
+  const result = validate(customer, customerSchema);
+  expect(result).toMatchSnapshot();
 });
--- src/validate/validateObject.ts
+++ src/validate/validateObject.ts
@@ -3,7 +3,19 @@ import { ObjectSchema } from "../models/schema/ObjectSchema";
 import { isNil } from "../utils/typeChecker";
 import { _validate } from "./validate";
 
-export const validateObject = <T extends { [key: string]: any }, R>(o: T | null, root: R, schema: ObjectSchema<T, R>): ObjectResult<T> => {
+export const isRegex = (str: string) => {
+  return str.startsWith("/") && str.endsWith("/");
+};
+
+export const getRegex = (str: string) => {
+  return new RegExp(`^${str.substr(1, str.length - 1)}$`);
+};
+
+export const validateObject = <T extends { [key: string]: any }, R>(
+  o: T | null,
+  root: R,
+  schema: ObjectSchema<T, R>
+): ObjectResult<T> => {
   //
   const result: ObjectResult<T> = {
     isValid: true,
@@ -25,15 +37,38 @@ export const validateObject = <T extends { [key: string]: any }, R>(o: T | null,
     return result;
   }
 
-  // for each key, validate
+  const obj = o as T;
+  const processedFields: string[] = [];
+
+  // for each static key, validate
   for (let field in schema.properties) {
+    if (isRegex(field)) continue;
     result.properties[field] = _validate(o ? o[field] : null, root, schema.properties[field]);
+    processedFields.push(field);
+  }
+
+  // for each regex in schema.properties
+  for (let field in schema.properties) {
+    if (!isRegex(field)) continue; // not-regex
+    const regex = getRegex(field);
+
+    // for each key
+    for (let key in obj) {
+      /**
+       * Already processed field. This will happen if the regex matches a wide range of fields, like ".*"
+       * and the properties also defines specific fields like "red": {}, "blue": {} etc ...
+       */
+      if (processedFields.includes(key)) continue;
+      if (!regex.test(key)) continue;
+      result.properties[key] = _validate(o ? o[key] : null, root, schema.properties[field]);
+      processedFields.push(key);
+    }
   }
 
   // if this node is valid, then check if all of it's children are valid
   // because the node is invalid, if any of it's children are invalid
   if (result.isValid) {
-    for (let field in schema.properties) {
+    for (let field of processedFields) {
       const property = result.properties[field];
       if (!property.isValid) {
         result.isValid = false;

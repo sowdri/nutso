ec5b898d6c1ccb813d3c583307c1c678b5f9fc44
Nish <n@elphe.net>
2025-04-09 16:54:13 +1000

    WIP

--- src/validate/validate.ts
+++ src/validate/validate.ts
@@ -36,55 +36,47 @@ export const _validate = <T, R, P>(args: {
    */
   switch (args.schema.type) {
     case "number":
-      return validateNumber({ ...args, schema: args.schema as NumberSchema<R> }) as Result<T>;
+      return validateNumber({
+        ...args,
+        schema: args.schema as NumberSchema<R>,
+      }) as Result<T>;
     case "string":
-      return validateString({ ...args, schema: args.schema as StringSchema<R> }) as Result<T>;
+      return validateString({
+        ...args,
+        schema: args.schema as StringSchema<R>,
+      }) as Result<T>;
     case "boolean":
-      return validateBoolean({ ...args, schema: args.schema as BooleanSchema<R> }) as Result<T>;
+      return validateBoolean({
+        ...args,
+        schema: args.schema as BooleanSchema<R>,
+      }) as Result<T>;
     case "array":
       // TODO array schema type remove any
-      return validateArray({ ...args, value: args.value as any, schema: args.schema as any }) as Result<T>;
+      return validateArray({
+        ...args,
+        value: args.value as any,
+        schema: args.schema as any,
+      }) as Result<T>;
     case "object":
       return validateObject({ ...args, schema: args.schema }) as Result<T>;
     case "date":
-      return validateDate({ ...args, schema: args.schema as DateSchema<R> }) as Result<T>;
+      return validateDate({
+        ...args,
+        schema: args.schema as DateSchema<R>,
+      }) as Result<T>;
   }
   throw new Error(`Unhandled data type`);
 };
 
 // public api
-export const validate = <T, R = T>(value: T, schema: Schema<T, R>): Result<T> => {
-  return _validate({ value, root: value as unknown as R, parent: null, schema });
+export const validate = <T, R = T>(
+  value: T,
+  schema: Schema<T, R>
+): Result<T> => {
+  return _validate({
+    value,
+    root: value as unknown as R,
+    parent: null,
+    schema,
+  });
 };
-
-// const api = <T, R = T>(args: { value: T; schema: Schema<T, R> }) => {
-//   return val({
-//     ...args,
-//     root: args.value as unknown as R,
-//     parent: null,
-//   });
-// };
-
-// const val = <T, P, R>(args: { value: T; parent: P; root: R; schema: Schema<T, R> }) => {
-//   if (!args.schema) {
-//     throw new Error("Schema should not be null");
-//   }
-//   switch (args.schema.type) {
-//     case "number":
-//       return validateNumber({ ...args, schema: args.schema as NumberSchema<R> }) as Result<T>;
-//     default:
-//       throw new Error(`Unhandled data type`);
-//     //   case "string":
-//     //     return validateString({ ...args, schema: args.schema as StringSchema<R> }) as Result<T>;
-//     //   case "boolean":
-//     //     return validateBoolean({ ...args, schema: args.schema as BooleanSchema<R> }) as Result<T>;
-//     //   case "array":
-//     //     return validateArray({ value: args.value as any, parent: args.parent, schema: args.schema as any }) as Result<T>;
-//     //   case "object":
-//     //     return validateObject({ ...args, schema: args.schema }) as Result<T>;
-//     //   case "date":
-//     //     return validateDate({ ...args, schema: args.schema as DateSchema<R> }) as Result<T>;
-//     // }
-//     // throw new Error(`Unhandled data type`);
-//   }
-// };
